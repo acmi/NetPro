8e788a179e7ede5f016e9f5a432bfdaafec3ec1e
_dev_ <cossacking@gmail.com>
2015-06-17 17:33:35 +0300

    Legacy item related packet updates Support for MR ticket - Double

---
+++ l2emuproject-netpro-app/scripts/interpreter/item/MonsterRaceLane.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2011-2015 L2EMU UNIQUE
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package interpreter.item;
+
+import net.l2emuproject.proxy.network.meta.interpreter.IntegerInterpreter;
+import net.l2emuproject.proxy.script.interpreter.ScriptedFieldValueInterpreter;
+import net.l2emuproject.proxy.state.entity.context.ICacheServerID;
+
+/**
+ * Interprets the lane number(s) on which the bet was placed.
+ * 
+ * @author _dev_
+ */
+public final class MonsterRaceLane extends ScriptedFieldValueInterpreter implements IntegerInterpreter
+{
+	@Override
+	public Object getInterpretation(long value, ICacheServerID entityCacheContext)
+	{
+		final long lane1 = value & 0x0F, lane2 = value >>> 4;
+		if (lane2 == 0)
+			return String.valueOf(value);
+		return lane1 + ", " + lane2;
+	}
+}

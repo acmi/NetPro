020f4aa68fd7c812f1b2dace7bdb56ded3d2c81c
_dev_ <cossacking@gmail.com>
2015-07-01 11:06:07 +0300

    Various legacy pledge related packet updates Includes CM ReqPledgePower and SM ManagePledgePower

---
+++ l2emuproject-netpro-app/scripts/interpreter/pledge/LegacyPledgeAuthority.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2011-2015 L2EMU UNIQUE
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package interpreter.pledge;
+
+import net.l2emuproject.proxy.script.interpreter.ScriptedBitmaskInterpreter;
+import net.l2emuproject.proxy.state.entity.context.ICacheServerID;
+
+/**
+ * @author _dev_
+ */
+public final class LegacyPledgeAuthority extends ScriptedBitmaskInterpreter
+{
+	/** Constructs this interpreter. */
+	public LegacyPledgeAuthority()
+	{
+		// @formatter:off
+		super(
+				"CRA/Join a Clan", "CRA/Give a title", "CRA/Viewing the Contents of a Warehouse", "CRA/Clan Crest Management", "CRA/Clan War",
+				"CHRA/Entrance Authority", "CHRA/Other Rights", "CHRA/Dismiss",
+				"CR/Opening a Door", "CR/Other Rights", "CR/Dismiss"
+		);
+		// @formatter:on
+	}
+	
+	@Override
+	public Object getInterpretation(long value, ICacheServerID entityCacheContext)
+	{
+		if (value == -1)
+			return "Pledge leader";
+			
+		return super.getInterpretation(value, entityCacheContext);
+	}
+}

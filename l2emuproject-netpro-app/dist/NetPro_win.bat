35c5b27f2eb9b986a50832232b4f5e6f40e7fbb7
_dev_ <cossacking@gmail.com>
2015-11-06 14:40:23 +0200

    Elevation check disabled by default
    (due to the nature of the application).
    Aside from that, LMP explicitly turned off for Windows until JFX
    transition is completed.
    Default heap size was doubled to better reflect the capabilities of
    current hardware configurations and to improve CMS performance when
    boxing many characters.

--- l2emuproject-netpro-app/dist/NetPro_win.bat
+++ l2emuproject-netpro-app/dist/NetPro_win.bat
@@ -1,5 +1,3 @@
 REM You may fine tune these parameters at will.
-REM 1G is fine for (at least) 9-boxing. After all, who plays with less than a full party of chars at once nowadays?
-REM You may specify a lower heap size, just keep in mind that once you set 128m (or lower), JavaFX internals may start interfering.
 
-@start /ABOVENORMAL javaw.exe -Xmn128m -Xmx1g -Xms1g -server -XX:+TieredCompilation -XX:+AggressiveOpts -XX:+UseConcMarkSweepGC -Djava.net.preferIPv4Stack=true -jar l2emuproject-netpro-app-1.3-SNAPSHOT.jar
+@start /ABOVENORMAL javaw.exe -server -Djava.net.preferIPv4Stack=true -Xmn256m -Xms2g -Xmx2g -XX:-UseLargePages -Dnet.l2emuproject/allow_elevation -XX:+UseConcMarkSweepGC -XX:+AggressiveOpts -XX:+TieredCompilation -jar l2emuproject-netpro-app-1.3-SNAPSHOT.jar

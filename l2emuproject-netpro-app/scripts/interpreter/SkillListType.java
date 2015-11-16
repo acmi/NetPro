ae6c24f3e0474bb17df09f7ca7f1ba7f791fd4e4
_dev_ <cossacking@gmail.com>
2015-11-16 21:52:42 +0200

    Next version: 1.4

--- l2emuproject-netpro-app/scripts/interpreter/SkillListType.java
+++ l2emuproject-netpro-app/scripts/interpreter/SkillListType.java
@@ -15,18 +15,35 @@
  */
 package interpreter;
 
-import net.l2emuproject.proxy.script.interpreter.ScriptedZeroBasedIntegerIdInterpreter;
+import java.util.HashMap;
+import java.util.Map;
+
+import net.l2emuproject.proxy.script.interpreter.ScriptedIntegerIdInterpreter;
 
 /**
  * Interprets the given byte/word/dword as a type of a skill list.
  * 
  * @author savormix
  */
-public final class SkillListType extends ScriptedZeroBasedIntegerIdInterpreter
+public final class SkillListType extends ScriptedIntegerIdInterpreter
 {
 	/** Constructs this interpreter. */
 	public SkillListType()
 	{
-		super("Class", "Fishing", "Pledge", "Pledge unit", null, "Certification", "Collection", null, null, "Skill linking");
+		super(makeMap());
+	}
+	
+	private static final Map<Long, ?> makeMap()
+	{
+		final Map<Long, String> result = new HashMap<>();
+		result.put(0L, "Class");
+		result.put(1L, "Fishing");
+		result.put(2L, "Pledge");
+		result.put(3L, "Pledge unit");
+		result.put(5L, "Certification");
+		result.put(6L, "Collection");
+		result.put(9L, "Linking");
+		result.put(57L, "Revelation of Chaos");
+		return result;
 	}
 }

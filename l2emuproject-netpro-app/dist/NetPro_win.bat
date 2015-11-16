76b877104e1ede8d7409b14f606d4e7745aafdbd
_dev_ <cossacking@gmail.com>
2015-11-16 14:09:21 +0200

    JavaFX target is version 2.0
    NP will be locked to memory by default

--- l2emuproject-netpro-app/dist/NetPro_win.bat
+++ l2emuproject-netpro-app/dist/NetPro_win.bat
@@ -1,3 +1,3 @@
 REM You may fine tune these parameters at will.
 
-@start /ABOVENORMAL javaw.exe -server -Djava.net.preferIPv4Stack=true -Xmn256m -Xms2g -Xmx2g -XX:-UseLargePages -Dnet.l2emuproject/allow_elevation -XX:+UseConcMarkSweepGC -XX:+AggressiveOpts -XX:+TieredCompilation -jar l2emuproject-netpro-app-1.3.jar
+@start /ABOVENORMAL javaw.exe -server -Djava.net.preferIPv4Stack=true -Xmn256m -Xms2g -Xmx2g -XX:+UseLargePages -Dnet.l2emuproject/allow_elevation -XX:+UseConcMarkSweepGC -XX:+AggressiveOpts -XX:+TieredCompilation -jar l2emuproject-netpro-app-2.0-SNAPSHOT.jar

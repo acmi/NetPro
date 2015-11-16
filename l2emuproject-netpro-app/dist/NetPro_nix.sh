f08787c420576ed7933898c05e1ba416ea889523
_dev_ <cossacking@gmail.com>
2015-11-16 13:44:42 +0200

    Version 1.3

--- l2emuproject-netpro-app/dist/NetPro_nix.sh
+++ l2emuproject-netpro-app/dist/NetPro_nix.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # You may fine tune these parameters at will.
 
-java -server -Djava.net.preferIPv4Stack=true -Xmn256m -Xms2g -Xmx2g -XX:+UseLargePages -Dnet.l2emuproject/allow_elevation -XX:+UseConcMarkSweepGC -XX:+AggressiveOpts -XX:+TieredCompilation -Djava.net.preferIPv4Stack=true -jar l2emuproject-netpro-app-1.3-SNAPSHOT.jar &
+java -server -Djava.net.preferIPv4Stack=true -Xmn256m -Xms2g -Xmx2g -XX:+UseLargePages -Dnet.l2emuproject/allow_elevation -XX:+UseConcMarkSweepGC -XX:+AggressiveOpts -XX:+TieredCompilation -Djava.net.preferIPv4Stack=true -jar l2emuproject-netpro-app-1.3.jar &

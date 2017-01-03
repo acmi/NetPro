0e1110412c6bc79a21a1eb5e6b3ecf0a36227c11
_dev_ <cossacking@gmail.com>
2017-01-03 15:42:58 +0200

    Next version: 1.7

--- l2emuproject-netpro-app/dist/NetPro_win.bat
+++ l2emuproject-netpro-app/dist/NetPro_win.bat
@@ -1,4 +1,4 @@
 REM You may fine tune these parameters at will.
 REM Please check the unix executable for more optimal parameters. These should ensure high compatibility.
 
-@start /ABOVENORMAL javaw.exe -Djava.net.preferIPv4Stack=true -Xmn64m -Xms512m -Xmx512m -Dnet.l2emuproject/allow_elevation -XX:+UseConcMarkSweepGC -jar l2emuproject-netpro-app-1.6.jar
+@start /ABOVENORMAL javaw.exe -Djava.net.preferIPv4Stack=true -Xmn64m -Xms512m -Xmx512m -Dnet.l2emuproject/allow_elevation -XX:+UseConcMarkSweepGC -jar l2emuproject-netpro-app-1.7-SNAPSHOT.jar
